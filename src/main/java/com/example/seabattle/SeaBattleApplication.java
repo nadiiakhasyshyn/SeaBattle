cd38f21d396217f280dd56e55521d046536cf39c
nadiiakhasyshyn <nadiia.khasyshyn.pm.2020@lpnu.ua>
2023-06-12 11:58:03 +0300

    changes

--- src/main/java/com/example/seabattle/SeaBattleApplication.java
+++ src/main/java/com/example/seabattle/SeaBattleApplication.java
@@ -2,13 +2,27 @@ package com.example.seabattle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 
 public class SeaBattleApplication {
     public static void main(String[] args) {
         SpringApplication.run(SeaBattleApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS");
+            }
+        };
     }
 }
 
